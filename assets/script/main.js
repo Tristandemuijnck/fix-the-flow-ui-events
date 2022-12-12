3b576714ee3ab51b1fd17276c8800e0f4f6d71c2
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-12-12 22:41:09 +0100

    8 completed - device rotate

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,30 +10,78 @@ function jumpHandler() {
 
 // Rotate
 let rotate = document.querySelector('a:nth-of-type(1)');
-rotate.addEventListener('click', () => {
+rotate.addEventListener('click', rotateHandler)
+rotate.addEventListener('animationend', rotateHandler)
+function rotateHandler() {
   rotate.classList.toggle('rotate')
-})
+}
 
 // Scale
 let scale = document.querySelector('a:nth-of-type(2)');
-scale.addEventListener('click', () => {
+scale.addEventListener('click', scaleHandler)
+scale.addEventListener('animationend', scaleHandler)
+function scaleHandler() {
   scale.classList.toggle('scale')
-})
+}
 
 // TranslateY
 let translateY = document.querySelector('a:nth-of-type(3)');
-translateY.addEventListener('click', () => {
+translateY.addEventListener('click', translateYHandler)
+translateY.addEventListener('animationend', translateYHandler)
+function translateYHandler() {
   translateY.classList.toggle('translateY')
-})
+}
 
 // TranslateX
 let translateX = document.querySelector('a:nth-of-type(4)');
-translateX.addEventListener('click', () => {
+translateX.addEventListener('click', translateXHandler)
+translateX.addEventListener('animationend', translateXHandler)
+function translateXHandler() {
   translateX.classList.toggle('translateX')
-})
+}
 
 // Keydown
 let keydown = document.querySelector('a:nth-of-type(5)');
-keydown.addEventListener('keydown', () => {
+keydown.addEventListener('keydown', keydownHandler)
+keydown.addEventListener('animationend', keydownHandler)
+function keydownHandler() {
   keydown.classList.toggle('keydown')
-})
+}
+
+// Keyup
+let keyup = document.querySelector('a:nth-of-type(6)');
+keyup.addEventListener('keyup', keyupHandler)
+keyup.addEventListener('animationend', keyupHandler)
+function keyupHandler() {
+  keyup.classList.toggle('keyup')
+}
+
+// Longpress
+let longpress = document.querySelector('a:nth-of-type(7)');
+let timeout;
+
+longpress.addEventListener('mousedown', function () {
+  timeout = setTimeout(function () {
+    longpressHandler();
+  }, 2000);
+});
+longpress.addEventListener('mouseup', function () {
+  clearTimeout(timeout);
+});
+longpress.addEventListener('animationend', longpressHandler)
+
+function longpressHandler() {
+  longpress.classList.toggle('longpress')
+}
+
+// Device rotate
+let deviceRotate = document.querySelector('a:nth-of-type(8)');
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', orientationHandler)
+  deviceRotate.addEventListener('animationend', orientationHandler)
+}
+
+function orientationHandler() {
+  deviceRotate.classList.toggle('deviceRotate')
+}
