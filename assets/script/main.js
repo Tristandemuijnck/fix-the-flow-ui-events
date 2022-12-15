da51429f3be266d8068ca20da5e218abe62e21fd
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-12-15 22:55:26 +0100

    Added hint + info
    Added hint + infotab
    Rotation fix

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,9 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
+
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+    interaction.classList.toggle('jump')
 }
 
 
@@ -12,48 +13,54 @@ function jumpHandler() {
 let rotate = document.querySelector('a:nth-of-type(1)');
 rotate.addEventListener('click', rotateHandler)
 rotate.addEventListener('animationend', rotateHandler)
+
 function rotateHandler() {
-  rotate.classList.toggle('rotate')
+    rotate.classList.toggle('rotate')
 }
 
 // Scale
 let scale = document.querySelector('a:nth-of-type(2)');
 scale.addEventListener('click', scaleHandler)
 scale.addEventListener('animationend', scaleHandler)
+
 function scaleHandler() {
-  scale.classList.toggle('scale')
+    scale.classList.toggle('scale')
 }
 
 // TranslateY
 let translateY = document.querySelector('a:nth-of-type(3)');
 translateY.addEventListener('click', translateYHandler)
 translateY.addEventListener('animationend', translateYHandler)
+
 function translateYHandler() {
-  translateY.classList.toggle('translateY')
+    translateY.classList.toggle('translateY')
 }
 
 // TranslateX
 let translateX = document.querySelector('a:nth-of-type(4)');
 translateX.addEventListener('click', translateXHandler)
 translateX.addEventListener('animationend', translateXHandler)
+
 function translateXHandler() {
-  translateX.classList.toggle('translateX')
+    translateX.classList.toggle('translateX')
 }
 
 // Keydown
 let keydown = document.querySelector('a:nth-of-type(5)');
 keydown.addEventListener('keydown', keydownHandler)
 keydown.addEventListener('animationend', keydownHandler)
+
 function keydownHandler() {
-  keydown.classList.toggle('keydown')
+    keydown.classList.toggle('keydown')
 }
 
 // Keyup
 let keyup = document.querySelector('a:nth-of-type(6)');
 keyup.addEventListener('keyup', keyupHandler)
 keyup.addEventListener('animationend', keyupHandler)
+
 function keyupHandler() {
-  keyup.classList.toggle('keyup')
+    keyup.classList.toggle('keyup')
 }
 
 // Longpress
@@ -61,44 +68,85 @@ let longpress = document.querySelector('a:nth-of-type(7)');
 let timeout;
 
 longpress.addEventListener('mousedown', function () {
-  timeout = setTimeout(function () {
-    longpressHandler();
-  }, 2000);
+    timeout = setTimeout(function () {
+        longpressHandler();
+    }, 2000);
 });
 longpress.addEventListener('mouseup', function () {
-  clearTimeout(timeout);
+    clearTimeout(timeout);
 });
 longpress.addEventListener('animationend', longpressHandler)
 
 function longpressHandler() {
-  longpress.classList.toggle('longpress')
+    longpress.classList.toggle('longpress')
 }
 
 // Double click
 let doubleClick = document.querySelector('a:nth-of-type(8)');
 doubleClick.addEventListener('dblclick', doubleClickHandler)
 doubleClick.addEventListener('animationend', doubleClickHandler)
+
 function doubleClickHandler() {
-  doubleClick.classList.toggle('dblClick')
+    doubleClick.classList.toggle('dblClick')
 }
 
 // Focus
 let focus = document.querySelector('a:nth-of-type(9)');
 focus.addEventListener('focus', focusHandler)
 focus.addEventListener('animationend', focusHandler)
+
 function focusHandler() {
-  focus.classList.toggle('focus')
+    focus.classList.toggle('focus')
 }
 
 // Device rotate
+// let deviceRotate = document.querySelector('a:nth-of-type(10)');
+
+// if (window.DeviceOrientationEvent) {
+//   window.addEventListener('deviceorientation', orientationHandler)
+//   deviceRotate.addEventListener('animationend', orientationHandler)
+// }
+
+// function orientationHandler() {
+//   deviceRotate.classList.toggle('deviceRotate')
+// }
+
+// Device motion
 let deviceRotate = document.querySelector('a:nth-of-type(10)');
 
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', orientationHandler)
-  deviceRotate.addEventListener('animationend', orientationHandler)
+deviceRotate.addEventListener('click', function () {
+    if ('DeviceOrientationEvent' in window && typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission().then(permissionState => {
+            if (permissionState === 'granted') {
+                addDeviceOrientationListener()
+            }
+        })
+    } else {
+        addDeviceOrientationListener()
+    }
+})
+
+function addDeviceOrientationListener() {
+    window.addEventListener('deviceorientation', function (event) {
+        deviceRotate.classList.toggle('deviceRotate')
+        console.log(event);
+    })
 }
 
-function orientationHandler() {
-  deviceRotate.classList.toggle('deviceRotate')
-}
+deviceRotate.addEventListener('animationend', function () {
+    deviceRotate.classList.toggle('deviceRotate')
+})
 
+
+// Hint button
+
+let hintBtn = document.querySelector('.hint');
+let infoTab = document.querySelector('.info-tab');
+
+hintBtn.addEventListener('click', hintHandler)
+infoTab.addEventListener('click', hintHandler)
+
+function hintHandler() {
+    infoTab.classList.toggle('hidden')
+    hintBtn.classList.toggle('hidden')
+}
